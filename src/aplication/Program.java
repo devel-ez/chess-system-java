f1b60e947fe2efe8b01083a95259e61ea6d02651
devel-ez <felipevelez.adm@hotmail.com>
2021-02-15 22:21:48 -0300

    primeiro commit

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,17 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package aplication;
 
-/**
- *
- * @author Mateus
- */
+import boardgame.Board;
+import chess.ChessMatch;
+
 public class Program {
     public static void main(String[] args) {
     
+    ChessMatch chessMatch = new ChessMatch ();
     
+    UI.printBoard(chessMatch.getPieces());
     }
 }
---
+++ src/boardgame/Piece.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boardgame;
+
+/**
+ *
+ * @author Mateus
+ */
+public class Piece {
+
+    protected Position position;
+    private Board board;
+
+    public Piece(Board board) {
+        this.board = board;
+    }
+
+    protected Board getBoard() {
+        return board;
+    }
+
+}
